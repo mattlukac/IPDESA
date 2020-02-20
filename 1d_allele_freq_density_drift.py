304b75a6fd5fbc9ecdfa8937e7a448a04765d1b6
Matt Lukac <mattmlukac@gmail.com>
2020-02-20 11:14:51 -0800

    init commit for fenics scripts

---
+++ 1d_allele_freq_density_drift.py
@@ -0,0 +1,64 @@
+from fenics import *
+import numpy as np
+import sympy as sym
+
+T = 50.0			    # final time
+num_steps = 100		# number of time steps
+dt = T / num_steps	# time step size
+Nref = 100		    # reference effective population size
+N = 20             # effective population size
+nu = N/Nref         # relative effective population size
+epsilon = 0.0       # distance from 0 and 1
+p = 0.1             # initial frequency
+
+# Create mesh and define function space
+nx = 50
+mesh = IntervalMesh(nx, epsilon, 1-epsilon)
+V = FunctionSpace(mesh, 'P', 1)
+
+# Define boundary condition 
+#u_D = Expression('-t/(2*N)+nu', 
+#                     degree=2 , N=N, nu=nu, t=0)
+
+def boundary(x, on_boundary):
+    return on_boundary
+
+bc = DirichletBC(V, Constant(0.0), boundary)
+#bc = []
+
+# Define initial value
+u_0 = Expression('100*exp(-pow(100*(x[0]-p),2))/sqrt(pi)', 
+        N=N, p=p, degree=2)
+#u_n = interpolate(u_D, V)
+u_n = project(u_0, V)
+
+# Define variational problem
+u = TrialFunction(V)
+v = TestFunction(V)
+xx = Expression('x[0]*(1-x[0])', degree = 2, domain=mesh)
+
+a = u*v*dx + dt/(4*N) * grad(xx*u)[0]*grad(v)[0]*dx
+L = u_n*v*dx
+
+# Create VTK file for saving solution
+vtkfile = File('allele_freq_density/solution.pvd')
+
+# Time-stepping
+u = Function(V)
+t = 0
+for _ in range(num_steps):
+    
+    # Update current time
+    t += dt
+    
+    # Compute solution and integrate to 1
+    solve(a == L, u, bc)
+    u.vector()[:] = nx*u.vector()[:] / np.sum(u.vector()[:])
+
+    # Save to file and plot solution
+    vtkfile << (u, t)
+    plot(u)
+
+    # Update previous solution
+    u_n.assign(u)
+
---
+++ 2d_wave1.py
@@ -0,0 +1,70 @@
+"""
+2-dimensional wave equation
+
+  u_t = c^2 Laplacian(u) + f  in a square domain
+  u_D = 0                     on the boundary 3 by 3 square
+  u_0 = xy(3-x)(3-y)          initial condition
+  du_0= 0                     initial velocity
+
+The initial condition u_0 is chosen as a hump.
+"""
+
+from __future__ import print_function
+from fenics import *
+import time
+
+T = 10.0            # final time
+num_steps = 100     # number of time steps
+dt = T / num_steps  # time step size
+c = 3.0             # wave constant
+#A = 1.0            # wave amplitude multiplier
+
+# Create mesh and define function space
+nx = ny = 30
+mesh = RectangleMesh(Point(0, 0), Point(3, 3), nx, ny)
+V = FunctionSpace(mesh, 'P', 1)
+
+# Define boundary condition
+#def boundary(x, on_boundary):
+#    return x[0] == 0 or x[0] == 3 or x[1] == 0 or x[1] == 3
+
+bc = DirichletBC(V, Constant(0.0), 'on_boundary')
+
+# Define initial value
+u_0 = Expression('0.5*x[0]*x[1]*(3-x[0])*(3-x[1])',
+                 degree=2)
+u_n0 = project(u_0, V)
+u_n1 = project(u_0, V)
+
+# Define variational problem
+u = TrialFunction(V)
+v = TestFunction(V)
+C = Constant(c)
+f = Expression('c*c*cos(c*t)*( x[0]*(3-x[0]) + x[1]*(3-x[1]) - 0.5*x[0]*x[1]*(3-x[0])*(3-x[1]) )',
+               degree = 2, t = 0, c = c)
+
+a = u*v*dx + dt**2 * C**2 * inner(grad(u), grad(v))*dx
+L = Constant(2.0)*u_n1*v*dx - u_n0*v*dx + dt**2 * f*v*dx
+
+# Create VTK file for saving solution
+vtkfile = File('wave/solution.pvd')
+
+# Time-stepping
+u = Function(V)
+t = 0
+for n in range(num_steps):
+
+    # Compute solution
+    solve(a == L, u, bc)
+
+    # Save to file and plot solution
+    vtkfile << (u, t)
+    plot(u)
+
+    # Update previous solutions
+    u_n0.assign(u_n1)
+    u_n1.assign(u)
+
+    # Update current time
+    t += dt
+    f.t = t
