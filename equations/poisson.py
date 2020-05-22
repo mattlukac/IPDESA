587cdf69460f18b2e18442e95e5826afc312a0fe
Matt Lukac <mattmlukac@gmail.com>
2020-05-22 16:24:21 -0700

    changed f to c

--- equations/poisson.py
+++ equations/poisson.py
@@ -1,19 +1,19 @@
 """
 Defines the following Poisson equation problem:
-  -Laplace(u) = f    on [0,1]
+  -Laplace(u) = c    on [0,1]
          u(0) = u_0  
          u(1) = u_1
-            f = constant
+            c = constant
 The above formulation gives an exact solution of
-         u(x) = -(f/2)x^2 + (f/2 + u_1 - u_0)x + u_0
+         u(x) = -(c/2)x^2 + (c/2 + u_1 - u_0)x + u_0
 
-Here we have Theta = (u0, u1, f).
+Here we have Theta = (u0, u1, c).
 """
 
 import numpy as np 
 
 # list for plot titles
-Theta_names = [r'$u_0$', r'$u_1$', r'$f$']
+Theta_names = [r'$u_0$', r'$u_1$', r'$c$']
 
 # DOMAIN
 def domain():
@@ -26,13 +26,13 @@ def domain():
 # SOLUTION
 def solution(Theta):
     """
-    Computes the solution u where Theta=[u0, u1, f]
+    Computes the solution u where Theta=[u0, u1, c]
     """
     x = domain()
     u0 = Theta[0]
     u1 = Theta[1]
-    f = Theta[2]
-    u = -f * x**2 / 2 + (f/2 + u1 - u0)*x + u0
+    c = Theta[2]
+    u = -c * x**2 / 2 + (c/2 + u1 - u0)*x + u0
     return u
 
 # THETA
@@ -41,9 +41,9 @@ def Theta(replicates):
     Simulates replicates of Theta
     Output has shape (replicates, numParams)
     """
-    # create replicates of Theta=[u0,u1,f]
+    # create replicates of Theta=[u0,u1,c]
     u0 = np.random.uniform(-10,10, size=replicates)
     u1 = np.random.uniform(-10,10, size=replicates)
-    f = np.random.uniform(-100,100, size=replicates)
-    Theta = np.stack((u0, u1, f), axis=1)
+    c = np.random.uniform(-100,100, size=replicates)
+    Theta = np.stack((u0, u1, c), axis=1)
     return Theta
