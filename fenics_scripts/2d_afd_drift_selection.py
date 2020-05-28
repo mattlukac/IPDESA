44ba911f4e3a3dabb787d523f296b6120e8e6929
Matt Lukac <mattmlukac@gmail.com>
2020-05-28 00:25:33 -0700

    recovered fenics_scripts

---
+++ fenics_scripts/2d_afd_drift_selection.py
@@ -0,0 +1,76 @@
+from fenics import *
+from ufl import diag_vector
+import numpy as np
+import sympy as sym
+
+T = 60.0	    # final time
+num_steps = 60     # number of time steps
+dt = T / num_steps  # time step size
+N1, N2 = 20, 20     # effective population size for pop 1 and 2
+Nref = 100          # reference effective population size
+nu1, nu2 = N1/Nref, N2/Nref         # relative effective population size
+epsilon = 0.0       # distance from 0 and 1
+p1, p2 = 0.5, 0.5   # initial x and y frequencies
+s1, s2 = 100, 100    # 1/(root(2)sigma_x) and 1/(root(2)sigma_y)
+g1, g2 = 0.04, -0.02   # 2Ns for each population
+
+# Create mesh and define function space
+n1, n2 = 50, 50
+mesh = RectangleMesh(Point(epsilon, epsilon), Point(1-epsilon, 1-epsilon), n1, n2)
+V = FunctionSpace(mesh, 'P', 1)
+
+# Define boundary condition 
+#u_D = Expression('-t/(2*N)+nu', 
+#                     degree=2 , N=N, nu=nu, t=0)
+
+def boundary(x, on_boundary):
+    return on_boundary
+
+bc = DirichletBC(V, Constant(0.0), boundary)
+#bc = []    # no boundary condition
+
+# Define initial value of Gaussian with low variance
+u_0 = Expression('s1*s2*exp(-(pow(s1*(x[0]-p1), 2) + pow(s2*(x[1]-p2), 2)))/pi', 
+        s1=s1, s2=s2, p1=p1, p2=p2, degree=2)
+#u_n = interpolate(u_D, V)
+u_n = project(u_0, V)
+
+# Define variational problem
+u = TrialFunction(V)
+v = TestFunction(V)
+
+# Drift term
+drift = Expression(('x[0]*(1-x[0])/N1', 'x[1]*(1-x[1])/N2'),
+          N1=N1, N2=N2, degree = 2, domain=mesh)
+
+# Selection term with time dependent 2Ns
+sel = Expression(('g1*x[0]*(1-x[0])', 
+    'g2*x[1]*(1-x[1])'),
+    g1=g1, g2=g2, degree = 2, domain=mesh)
+
+a = u*v*dx + dt/4 * inner(diag_vector(grad(drift*u)), grad(v))*dx - dt*u*inner(sel, grad(v))*dx 
+L = u_n*v*dx
+
+# Create VTK file for saving solution
+vtkfile = File('solutions/2d_allele_freq_density_drift_selection/solution.pvd')
+
+# Time-stepping
+u = Function(V)
+t = 0
+for _ in range(num_steps):
+    
+    # Update current time
+    t += dt
+    sel.t = t
+    
+    # Compute solution and integrate to 1
+    solve(a == L, u, bc)
+    u.vector()[:] = n1*n2*u.vector()[:] / np.sum(u.vector()[:])
+
+    # Save to file and plot solution
+    vtkfile << (u, t)
+    plot(u)
+
+    # Update previous solution
+    u_n.assign(u)
+
---
+++ fenics_scripts/2d_wave2.py
@@ -0,0 +1,83 @@
+"""
+2-dimensional wave equation
+
+  u_t = c^2 Laplacian(u) + f  in a square domain
+  u_D = 0                     on the boundary 3 by 3 square
+  u_0 = xy(3-x)(3-y)sin(3x)cos(3y)         initial condition
+  du_0= 0                     initial velocity
+
+The initial condition u_0 is chosen as a hump.
+"""
+
+from __future__ import print_function
+from fenics import *
+import time
+import sympy as sym
+
+T = 10.0            # final time
+num_steps = 100     # number of time steps
+dt = T / num_steps  # time step size
+c = 3.0             # wave constant
+#A = 1.0            # wave amplitude multiplier
+
+# Create mesh and define function space
+nx = ny = 30
+mesh = RectangleMesh(Point(0, 0), Point(3, 3), nx, ny)
+V = FunctionSpace(mesh, 'P', 1)
+
+# Define boundary condition
+#def boundary(x, on_boundary):
+#    return x[0] == 0 or x[0] == 3 or x[1] == 0 or x[1] == 3
+
+bc = DirichletBC(V, Constant(0.0), 'on_boundary')
+
+# Define initial value
+x, y, sx, sy, cx, cy = sym.symbols('x[0] x[1] sin(3*x[0]) sin(3*x[1]) cos(3*x[0]) cos(3*x[1])')
+
+u_0 = 0.5*x*y*(3-x)*(3-y)*sx*cy
+u_0 = sym.printing.ccode(u_0)
+u_0 = Expression(u_0, degree = 2)
+
+u_n0 = project(u_0, V)
+u_n1 = project(u_0, V)
+
+# Define variational problem
+u = TrialFunction(V)
+v = TestFunction(V)
+C = Constant(c)
+
+# f is u_tt - c^2 Lap(u)
+t, ct = sym.symbols('t cos(c*t)')
+f = -0.5*c**2*ct*( 
+        x*y*(3-x)*(3-y)*sx*cx + 
+        y*(3-y)*cy*( 6*(3-2*x)*cx - sx*(2+9*x*(3-x)) ) + 
+        x*(3-x)*sx*( cy*(9*y*(3-y)-2) - 6*(3-2*y)*sy ) 
+        )
+f = sym.printing.ccode(f)
+f = Expression(f, degree = 2, t = 0, c = c)
+
+a = u*v*dx + dt**2 * C**2 * inner(grad(u), grad(v))*dx
+L = Constant(2.0)*u_n1*v*dx - u_n0*v*dx + dt**2 * f*v*dx
+
+# Create VTK file for saving solution
+vtkfile = File('solutions/2d_wave2/solution.pvd')
+
+# Time-stepping
+u = Function(V)
+t = 0
+for n in range(num_steps):
+
+    # Compute solution
+    solve(a == L, u, bc)
+
+    # Save to file and plot solution
+    vtkfile << (u, t)
+    plot(u)
+
+    # Update previous solutions
+    u_n0.assign(u_n1)
+    u_n1.assign(u)
+
+    # Update current time
+    t += dt
+    f.t = t
